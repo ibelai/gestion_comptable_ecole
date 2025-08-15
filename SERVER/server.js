eff048b6f973b68c41e469b933ba7212bb6591ce
ibBamba <ibrahimabamba83@gmail.com>
2025-08-15 16:44:33 +0000

    Mise à jour du projet

--- SERVER/server.js
+++ SERVER/server.js
@@ -42,4 +42,7 @@ app.use("/api/dashboard", Dashboard);
 
 
 // Démarrer le serveur sans Sequelize
-app.listen(5000, () => console.log("✅ Serveur démarré sur le port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
