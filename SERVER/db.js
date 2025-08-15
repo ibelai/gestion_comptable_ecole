274dbe096c95914a7558a513e1b3dd78413f4e8e
ibBamba <ibrahimabamba83@gmail.com>
2025-08-15 20:44:43 +0000

    Mise à jour du projet

--- SERVER/db.js
+++ SERVER/db.js
@@ -5,7 +5,8 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,        // depuis Render Environment Variables
   user: process.env.DB_USER,        // depuis Render Environment Variables
   password: process.env.DB_PASSWORD,// depuis Render Environment Variables
-  database: process.env.DB_NAME,    // depuis Render Environment Variables
+  database: process.env.DB_NAME,
+  port: process.env.PORT || 5000,  // depuis Render Environment Variables
   ssl: { rejectUnauthorized: true } // requis si tu utilises PlanetScale ou autre DB cloud
 });
 
