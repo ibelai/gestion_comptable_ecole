4b881978962c5590626306c304bc1bba078b25d1
ibBamba <ibrahimabamba83@gmail.com>
2025-08-16 09:53:36 +0000

    Mise à jour du projet

--- SERVER/server.js
+++ SERVER/server.js
@@ -62,7 +62,7 @@ app.use("/api/paiements", paiementsRoutes);
 app.use("/api/dashboard", Dashboard);
 
 // Démarrer le serveur Express
-const port = process.env.PORT || 5000;
+const port = process.env.PORT
 app.listen(port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
 });
