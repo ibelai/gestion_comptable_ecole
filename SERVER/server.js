faa6423c154e20afcedde9c15f4b7d58e03570f2
ibelai <bambaibrahima4419@gmail.com>
2025-08-20 21:29:29 +0000

    Mettre a jour

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,22 +1,34 @@
+// Charger les variables d'environnement
 require("dotenv").config();
 const path = require('path');
-
 const express = require("express");
 const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
 
+// Récupération des variables d'environnement
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+const jwtSecret = process.env.JWT_SECRET;
+
+
+
+// Initialisation d'Express
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Dossiers statiques pour les uploads
 app.use("/uploads", express.static("uploads"));
 app.use('/uploads/recus', express.static('uploads/recus'));
 
-app.use(express.urlencoded({ extended: true }));
 // Routes
+const authRoutes = require("./routes/authRoutes");
 const profilRoute = require("./routes/profile");
 const uploadRouter = require("./routes/upload");
 const userRoutes = require("./routes/userRoutes");
-const elevesRoutes = require('./routes/elevesRoutes')
+const elevesRoutes = require('./routes/elevesRoutes');
 const changerMotdePase = require('./routes/changerMotDePasse');
 const classesRoutes = require('./routes/classeRoutes');
 const registerRoute = require("./routes/registerRoutes");
@@ -25,7 +37,7 @@ const MontantClasse = require("./routes/montant_classeRoutes");
 const Dashboard = require("./routes/Dashboard");
 const userRouter = require("./routes/user");
 
-
+// Définition des routes
 app.use('/api/eleves', elevesRoutes);
 app.use('/api/montants-classes', MontantClasse);
 app.use("/api/users", userRoutes);
@@ -39,7 +51,8 @@ app.use("/api/motPasse", changerMotdePase);
 app.use("/api/paiements", paiementsRoutes);
 app.use("/api/dashboard", Dashboard);
 
-
-
-// Démarrer le serveur sans Sequelize
-app.listen(1000, () => console.log("✅ Serveur démarré sur le port 5000"));
+// Démarrer le serveur Express
+const port = process.env.PORT || 1000
+app.listen(port, () => {
+  console.log(`Serveur démarré sur le port ${port}`);
+});
