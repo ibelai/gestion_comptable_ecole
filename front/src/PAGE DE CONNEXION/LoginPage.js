d4d4b224fedd65f04d1f82232f58979d43c7772c
ibelai <bambaibrahima4419@gmail.com>
2025-08-21 15:46:59 +0000

    Mettre a jour

--- front/src/PAGE DE CONNEXION/LoginPage.js
+++ front/src/PAGE DE CONNEXION/LoginPage.js
@@ -8,7 +8,7 @@ function Login({ onLoginSuccess }) {
   const [error, setError] = useState("");
 
   const navigate = useNavigate();
-const API_URL =process.env.REACT_APP_API_URL;
+const API_URL =process.env.REACT_APP_API_URL || "http://localhost:1000"; // Assurez-vous que cette variable d'environnement est définie
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
